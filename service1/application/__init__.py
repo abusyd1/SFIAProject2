9b9d13f96b733521dba6387ecb0c92aa06366af1
Sayeed <abusayeed16@gmail.com>
2021-02-19 18:24:35 +0000

    playing around

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 from flask import Flask
-from os import getenv
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.83.244.57/Project22"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
-
 
 from application import routes
