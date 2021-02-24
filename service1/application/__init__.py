2340c7203c046f3fd6614fc860a278611ed02c43
Sayeed <abusayeed16@gmail.com>
2021-02-24 16:26:54 +0000

    Cannot get service4 to work :(

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-
+  
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-SECRET_KEY = os.urandom(32)
-
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.105.84.9/flask_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testdb.sqlite"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
 
-from application import routes
+from application import routes                 
