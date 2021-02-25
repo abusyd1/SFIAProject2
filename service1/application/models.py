8bc9b5d4605fb61bbca2b449fb0d6082b7b23cfa
Sayeed <abusayeed16@gmail.com>
2021-02-25 12:40:06 +0000

    same place

--- service1/application/models.py
+++ service1/application/models.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from application import db
 #from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, DateField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, NumberRange, Length
-from datetime import datetime
 
 class Player(db.Model):
     Id=db.Column(db.Integer, primary_key=True)
     Player_position=db.Column(db.String(30), nullable=False)
     Player_nationality=db.Column(db.String(30), nullable=False)
     Player_profile= db.Column(db.String(3000), nullable=False)
-    Date=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.Id} | {self.Player_position} | {self.Player_nationlity}| {self.Player_profile}"
