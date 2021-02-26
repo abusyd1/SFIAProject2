bce21af422188455aca7895a14d919308e0e7b30
Sayeed <abusayeed16@gmail.com>
2021-02-26 10:07:14 +0000

    MYSQL Connectivity

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testdb.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.105.84.9/flask_db"
+app.config['SECRET_KEY'] = 'ewfkjkjdfsd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
