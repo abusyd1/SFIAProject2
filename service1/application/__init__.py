0419a344c271f5f28cea9006664ad2750f28bbf8
Sayeed <abusayeed16@gmail.com>
2021-02-27 15:40:09 +0000

    Environment Variables

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -2,12 +2,12 @@
   
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from os import getenv
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.105.84.9/flask_db"
-app.config['SECRET_KEY'] = 'ewfkjkjdfsd'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URI')
+app.config['SECRET_KEY'] = getenv('SEC_KEY')
 db = SQLAlchemy(app)
 
 from application import routes                 
