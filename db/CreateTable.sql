10f48296632ec54a68b58d028c2f86d2b40f09ee
Sayeed <abusayeed16@gmail.com>
2021-03-01 21:02:46 +0000

    Stack working V2

--- db/CreateTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Player
-             (
-                          id         INTEGER NOT NULL,
-                          Player_position VARCHAR(30) NOT NULL,
-                          Player_nationality  VARCHAR(30) NOT NULL,
-			  Player_profile VARCHAR(3000), NOT NULL,
-                          PRIMARY KEY (id)
-);
