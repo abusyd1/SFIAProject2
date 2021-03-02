25b76877ad6c1b8aa90ef7636a21be710c2641bd
Sayeed <abusayeed16@gmail.com>
2021-03-02 11:39:17 +0000

    I'A

--- jenkins/build-images.sh
+++ jenkins/build-images.sh
@@ -1,14 +1,12 @@
+# SSH into manager
+#! /bin/bash
+ssh 10.138.0.11 << EOF 
 # Remove any previous locally built images 
 docker-compose down --rmi local
-
-
 # build the images for the services from the docker-compose.yaml
-docker-compose build 
-
-
+docker-compose build
 # log into docker with the account credentials  
-sudo docker login 
-
-
+sudo docker login -u abusyd1 -p TempPass2021
 # push our built images to dockerhub
 sudo docker-compose push
+EOF
