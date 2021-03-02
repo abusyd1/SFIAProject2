a138ae0d94fa4ea68325e3e1d401068f3d205edc
Sayeed <abusayeed16@gmail.com>
2021-03-02 12:17:05 +0000

    trying

--- jenkins/build-images.sh
+++ jenkins/build-images.sh
@@ -1,7 +1,7 @@
 # SSH into manager
 #! /bin/bash
-sh 'ssh -o StrictHostKeyChecking=no root@10.138.0.11'
-ssh 10.138.0.11 << EOF 
+
+ssh -T -i  ~/.ssh/key.pem -o StrictHostKeyChecking=no abusayeed_cb@10.138.0.11 << EOF 
 # Remove any previous locally built images 
 docker-compose down --rmi local
 # build the images for the services from the docker-compose.yaml
