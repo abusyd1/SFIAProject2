5b8dd0e827b7f265677d5ed6ab912d0e16daeea0
Sayeed <abusayeed16@gmail.com>
2021-03-02 12:34:12 +0000

    .

--- jenkins/deploy-services.sh
+++ jenkins/deploy-services.sh
@@ -4,7 +4,7 @@ sudo rm -rf SFIAProject2
   
 git clone https://github.com/abusyd1/SFIAProject2.git
 cd SFIAProject2
-sudo docker login
+sudo docker login -u abusyd1 -p TempPass2021
  
  
 sudo docker stack deploy --compose-file docker-compose.yaml generator
